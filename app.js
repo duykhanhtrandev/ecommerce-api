a2585add59895310a3fb84e05416c353ca3bb5a4
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-05-27 10:53:50 +0700

    setup project structure

--- app.js
+++ app.js
@@ -3,23 +3,32 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+// rest of the packages
+const morgan = require("morgan");
+
 // connectDB
 const connectDB = require("./db/connect");
+
+// routers
+const authRouter = require("./routes/authRoutes");
 
 // middleware
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(morgan("tiny"));
 app.use(express.json());
 
 app.use("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use("/api/v1/auth", authRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = 5000 || process.env.PORT;
+const port = 3000 || process.env.PORT;
 
 const start = async () => {
   try {

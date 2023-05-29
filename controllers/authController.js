1a5324eadaf72271c4acab115e0b09b7f0d04d5a
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-05-29 09:25:11 +0700

    hash and compare password

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,12 +3,17 @@ const User = require("../models/userModel");
 const CustomError = require("../errors/");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { name, email, password } = req.body;
   const emailAlreadyExists = await User.findOne({ email });
   if (emailAlreadyExists) {
     throw new CustomError.BadRequestError("Email already exist!");
   }
-  const user = await User.create(req.body);
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ name, email, password, role });
   res.status(StatusCodes.CREATED).json({ user });
 };
 

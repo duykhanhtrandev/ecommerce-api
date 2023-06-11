d7c4f39e32a58f74c030314be42018f2a48389b5
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-06-11 13:59:22 +0700

    add populate method and mongoose virtuals for reviews of a product

--- models/productModel.js
+++ models/productModel.js
@@ -63,7 +63,19 @@ const ProductSchema = new mongoose.Schema(
       required: true,
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
 );
+
+ProductSchema.virtual("reviews", {
+  ref: "Review",
+  localField: "_id",
+  foreignField: "product",
+  justOne: false,
+  // match: { rating: 4.5 },
+});
 
 module.exports = mongoose.model("Product", ProductSchema);

d31edcfc9845f01f385f5bf38e6981f181b06383
Duy Khánh <109634649+duykhanhtrandev@users.noreply.github.com>
2023-06-13 10:40:19 +0700

    remove all reviews with pre

--- controllers/productController.js
+++ controllers/productController.js
@@ -39,10 +39,11 @@ const updateProduct = async (req, res) => {
 
 const deleteProduct = async (req, res) => {
   const { id: productId } = req.params;
-  const product = await Product.deleteOne({ _id: productId });
+  const product = await Product.findOne({ _id: productId });
   if (!product) {
     throw new CustomError.NotFoundError(`No product with id: ${productId}`);
   }
+  await product.deleteOne();
   res.status(StatusCodes.OK).json({ msg: "Success! Product Removed" });
 };
 
